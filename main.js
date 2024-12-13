e0c54163a87dde5afb3fa5876505c80781667cda
Federico Nova <fedenova8@gmail.com>
2024-12-13 20:37:14 -0300

    Hicimos que, al apretar los botones, aparezca secuencialmente el gato correspondiente

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 import './style.css'
+
+let gatoUno = '😺'
+let gatoDos = '😸'
+let gatoTres = '😹'
+
+let botonUno = document.getElementById('gato-uno')
+let botonDos = document.getElementById('gato-dos')
+let botonTres = document.getElementById('gato-tres')
+
+let contenedor = document.createElement('div')
+document.body.appendChild(contenedor)
+
+let gatos = document.createElement('p')
+
+botonUno.addEventListener('click', function() {
+    if (contenedor.textContent == '') {
+        contenedor.textContent = gatoUno
+        console.log(contenedor.textContent);
+    } else {
+        contenedor.textContent = (contenedor.textContent + gatoUno)
+        console.log(contenedor.textContent);
+    }
+    }
+)
+
+botonDos.addEventListener('click', function() {
+    if (contenedor.textContent == '') {
+        contenedor.textContent = gatoDos
+        console.log(contenedor.textContent);
+    } else {
+        contenedor.textContent = (contenedor.textContent + gatoDos)
+        console.log(contenedor.textContent);
+    }
+    }
+)
+
+botonTres.addEventListener('click', function() {
+    if (contenedor.textContent == '') {
+        contenedor.textContent = gatoTres
+        console.log(contenedor.textContent);
+    } else {
+        contenedor.textContent = (contenedor.textContent + gatoTres)
+        console.log(contenedor.textContent);
+    }
+    }
+)
