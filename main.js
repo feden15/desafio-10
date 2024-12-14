2bb91eca64adce8056a7962b30164513bca6a786
Federico Nova <fedenova8@gmail.com>
2024-12-13 21:35:11 -0300

    Terminamos el desafio 10

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import './style.css'
 let gatoUno = '😺'
 let gatoDos = '😸'
 let gatoTres = '😹'
+let caja = '📦'
 
 let botonUno = document.getElementById('gato-uno')
 let botonDos = document.getElementById('gato-dos')
@@ -11,37 +12,58 @@ let botonTres = document.getElementById('gato-tres')
 let contenedor = document.createElement('div')
 document.body.appendChild(contenedor)
 
-let gatos = document.createElement('p')
+let contadorUno = 0
+let contadorDos = 0
+let contadorTres = 0
+let contadorCajas = 0
 
 botonUno.addEventListener('click', function() {
-    if (contenedor.textContent == '') {
-        contenedor.textContent = gatoUno
-        console.log(contenedor.textContent);
-    } else {
+    if (contadorUno <= 4) {
         contenedor.textContent = (contenedor.textContent + gatoUno)
         console.log(contenedor.textContent);
+        contadorUno++
+        contadorDos = contadorTres = 0
+    } else {
+        contenedor.textContent = caja
+        for (let i=1; i<=contadorCajas; i++) {
+            contenedor.textContent = contenedor.textContent + caja
+        }
+        contadorCajas++
+        contadorUno = contadorDos = contadorTres = 0
     }
     }
 )
 
 botonDos.addEventListener('click', function() {
-    if (contenedor.textContent == '') {
-        contenedor.textContent = gatoDos
-        console.log(contenedor.textContent);
-    } else {
+    if (contadorDos <= 4) {
         contenedor.textContent = (contenedor.textContent + gatoDos)
         console.log(contenedor.textContent);
+        contadorDos++
+        contadorUno = contadorTres = 0
+    } else {
+        contenedor.textContent = caja
+        for (let i=1; i<=contadorCajas; i++) {
+            contenedor.textContent = contenedor.textContent + caja
+        }
+        contadorCajas++
+        contadorUno = contadorDos = contadorTres = 0
     }
     }
 )
 
 botonTres.addEventListener('click', function() {
-    if (contenedor.textContent == '') {
-        contenedor.textContent = gatoTres
-        console.log(contenedor.textContent);
-    } else {
+    if (contadorTres <= 4) {
         contenedor.textContent = (contenedor.textContent + gatoTres)
         console.log(contenedor.textContent);
+        contadorTres++
+        contadorDos = contadorTres = 0
+    } else {
+        contenedor.textContent = caja
+        for (let i=1; i<=contadorCajas; i++) {
+            contenedor.textContent = contenedor.textContent + caja
+        }
+        contadorCajas++
+        contadorUno = contadorDos = contadorTres = 0
     }
     }
 )
